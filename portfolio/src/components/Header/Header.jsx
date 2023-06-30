72753baf9008dd152709650a4559b14d6b8b5653
Devdenice <denicepelo3@gmail.com>
2023-06-30 22:02:40 +0800

    first version portfolio

---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,130 @@
+import React, { useRef, useEffect } from "react";
+
+const Header = () => {
+  // ========== For Sticky Header ============
+  const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
+
+  const stickyHeaderFunction = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunction();
+
+    return window.removeEventListener("scroll", stickyHeaderFunction);
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const targetAttr = e.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
+
+    window.scrollTo({
+      top: location - 80,
+      left: 0,
+    });
+  };
+  // ========== For Sticky Header End ============
+
+  const toggleMenu = () => menuRef.current.ClassList.toggle("show__menu");
+  return (
+    <header
+      ref={headerRef}
+      className="w-full h-[80px] leading-[80px] flex items-center"
+    >
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* ============ LOGO Start =========== */}
+          <div className="flex items-center gap-[10px]">
+            <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
+              D
+            </span>
+            <div className="leading-[20px]">
+              <h2 className="text-xl text-smallTextColor font-[700]">Denice</h2>
+              <p className="text-smallTextColor text-[14px] font-[500]">
+                Personal
+              </p>
+            </div>
+          </div>
+
+          {/* =============== LOGO End ============= */}
+
+          {/* =============== Menu Start =========== */}
+
+          <div className="menu" ref={menuRef} onClick={toggleMenu}>
+            <ul className="flex items-center gap-10">
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#about"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#services"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#portfolio"
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* =============== Menu End  =========== */}
+
+          {/* =============== Menu Right  =========== */}
+
+          <div className="flex items-center gap-4">
+            <button className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300 ">
+              <i class="ri-send-plane-line"></i>Let's talk
+            </button>
+
+            <span
+              onClick={toggleMenu}
+              className="text-2xl text-smallTextColor md:hidden  cursor-pointer"
+            >
+              <i class="ri-menu-fill"></i>
+            </span>
+          </div>
+          {/* =============== Menu End  =========== */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ portfolio/src/components/UI/Contact.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="pb-16">
+      <div className="container">
+        <h2 className="text-headingColor font-[700] text-[2.5rem] mb-8 ">
+          Get in touch
+        </h2>
+        <div className="md:flex justify-between items-center">
+          <div className="w-full md:w-1/2 h-[300px] sm:h-[450px]">
+            <iframe
+              title="google-maps"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.2805646280135!2d123.51935027345333!3d13.701449898460572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a1c312e0861b5b%3A0xfa3941217fa0b7c!2sPartido%20San%20Jose%20Camarines%20Sur!5e0!3m2!1sen!2sph!4v1687861876804!5m2!1sen!2sph"
+              className="border-0 w-full h-full"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
+          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8  ">
+            <form className="w-full" action="">
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <textarea
+                  type="text"
+                  rows={3}
+                  placeholder="Write your message"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
